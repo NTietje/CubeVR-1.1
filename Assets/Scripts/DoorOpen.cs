e9f79cbb43e7f6d1a6d8c4f9d1e44e29e05dff55
Nana <tietje.hh@gmail.com>
2017-10-13 20:51:11 +0200

    Version nach dem Rundgang

---
+++ Assets/Scripts/DoorOpen.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour {
+
+	public Door _Door;
+	public GameObject prefab;
+	public AudioSource sourceFX;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other) {
+
+        if (other.gameObject.name == "Sphere") {
+        _Door.DoorOpen();
+        Destroy(prefab);
+        sourceFX.Play();
+    }
+
+		}
+
+	/*void OnTriggerEnter (Collider other) {
+	
+		if (other.gameObject.name == "Sphere") 
+		{
+			
+		_Door.DoorOpen ();
+		Destroy (prefab);
+		sourceFX.Play();
+
+		}
+
+	}*/
+}
---
+++ Assets/Scripts/SphereMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereMovement : MonoBehaviour
+{
+
+    public float value = 0;
+    public GameObject prefab;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+   /* void OnCollisionEnter (Collision col)
+	{
+
+		prefab.GetComponent<Rigidbody> ().angularDrag = value;
+
+	}*/
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.name == "Sphere")
+        {
+            prefab.GetComponent<Rigidbody>().angularDrag = value;
+        }
+
+    }
+}
